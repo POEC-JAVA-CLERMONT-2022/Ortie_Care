e9fcbe6f283bd13630c5689746ca0a1f5eaaed8b
Olivia <laconte.olivia@sfr.fr>
2022-03-29 14:59:20 +0200

    ajout userRepository

--- src/main/java/com/ipme/ortiecare/repository/UserRepository.java
+++ src/main/java/com/ipme/ortiecare/repository/UserRepository.java
@@ -1,16 +1,8 @@
 package com.ipme.ortiecare.repository;
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 
 import com.ipme.ortiecare.model.*;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-	
-@Query("select * from users where id_user = :id")
-User findByUUID(@Param("id")UUID id);
-
-@Query("select is_admin from users where id_user = :id")
-boolean isThisIDAdmin(@Param("id")UUID id);
+public class UserRepository extends JpaRepository {
+ 
 }
