9cb538714b6d21a5f7f5d9fb3f2e58965379b8c4
Olivia <laconte.olivia@sfr.fr>
2022-03-30 11:36:10 +0200

    config spring

--- src/main/java/com/ipme/ortiecare/repository/ConseilDeCultureRepository.java
+++ src/main/java/com/ipme/ortiecare/repository/ConseilDeCultureRepository.java
@@ -3,15 +3,17 @@ package com.ipme.ortiecare.repository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.ipme.ortiecare.model.ConseilDeCulture;
 
+@Repository
 public interface ConseilDeCultureRepository extends JpaRepository<ConseilDeCulture, UUID>{
 
-	@Query("SELECT * FROM conseils_de_culture where id_conseil = :id")
+	@Query(value="SELECT * FROM conseils_de_culture where id_conseil = :id", nativeQuery = true)
 	ConseilDeCulture findByUUID(@Param("id")UUID id_conseil);
 	
-	@Query("SELECT * FROM conseils_de_culture where titre = :titre")
+	@Query(value="SELECT * FROM conseils_de_culture where titre = :titre", nativeQuery = true)
 	ConseilDeCulture findByTitle(@Param("titre")String titre);
 	
 	
