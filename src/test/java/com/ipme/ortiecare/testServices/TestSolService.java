882e2b682f2bd8c195a6d4cc79c70cbe6dd1d175
Olivia <laconte.olivia@sfr.fr>
2022-04-14 15:35:27 +0200

    test services

--- src/test/java/com/ipme/ortiecare/testServices/TestSolService.java
+++ src/test/java/com/ipme/ortiecare/testServices/TestSolService.java
@@ -1,5 +1,38 @@
 package com.ipme.ortiecare.testServices;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ipme.ortiecare.model.Sol;
+import com.ipme.ortiecare.repository.SolRepository;
+import com.ipme.ortiecare.services.SolService;
+
 public class TestSolService {
+	
+	@Autowired
+	@InjectMocks
+	SolService solService;
+	
+	@Mock
+	private SolRepository mockedSolRepo;
+	
+	@Test
+	public void testCreationSol() {
+		when(mockedSolRepo.save(Mockito.any(Sol.class))).thenReturn(new Sol(null, null, null, null, null, null));
+		
+		Sol testSol = solService.create(null, null, null, null, null);
+		
+		Assertions.assertEquals(testSol.getId(), null);
+		assertThat(testSol).isNotNull();
+		assertThat(testSol).usingRecursiveComparison().isEqualTo(new Sol (null, null, null, null, null, null));
+		Mockito.verify(mockedSolRepo, times(1)).save(Mockito.any(Sol.class));
+		
+	}
 
 }
