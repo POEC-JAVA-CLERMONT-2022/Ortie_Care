73abd848bda1746a9be8f4dca13f574b28d0f79e
DESKTOP-K28699H\CGYORI-Vacances <charles.gyori@gmail.com>
2022-04-14 11:08:23 +0200

    Test live

--- src/test/java/com/ipme/ortiecare/testServices/testLegumeService.java
+++ src/test/java/com/ipme/ortiecare/testServices/testLegumeService.java
@@ -1,21 +1,43 @@
 package com.ipme.ortiecare.testServices;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ipme.ortiecare.model.ConseilDeCulture;
 import com.ipme.ortiecare.model.Legume;
+import com.ipme.ortiecare.model.Sol;
+import com.ipme.ortiecare.repository.LegumesRepository;
 import com.ipme.ortiecare.services.LegumeService;
 
+//@SpringBootTest
+//@RunWith(MockitoJUnitRunner.class)
+//@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class testLegumeService {
 
-	@Autowired
+//	@Autowired
+	@InjectMocks
 	LegumeService legumeService;
+	
+	@Mock
+	private LegumesRepository mockedLegumeRepo;
 	
 	@Test
 	public void testGetLegumesAssocies()
@@ -37,4 +59,36 @@ public class testLegumeService {
 //		assertNotNull(unUser);
 		
 	}
+//	public Legume create(String nom,int tempsAvantRecolteEnMois, int poidsMoyenFruitEnG, Set<ConseilDeCulture> conseilsDeCulture, boolean autoReseme, boolean isGousse, Sol bestSol)
+//	{
+//		Legume ceLegume = new Legume(UUID.randomUUID(), nom, tempsAvantRecolteEnMois, poidsMoyenFruitEnG ,conseilsDeCulture, autoReseme, isGousse, bestSol);
+//		this.legumesRepo.save(ceLegume);
+//		return ceLegume;
+//	}
+	
+	
+	// Test utilisation Mock
+	@Test
+	public void testCreationLegume()
+	{
+		when(mockedLegumeRepo.save(Mockito.any(Legume.class))).thenReturn(new Legume(null, null, 0, 0, null, false, false, null));
+		Legume testLegume = legumeService.create(null, 0, 0, null, false, false, null);
+		// test null
+		assertNotNull(testLegume);
+		// test id null comme dans le test
+		Assertions.assertEquals(testLegume.getIdLegume(), null);
+		// test objet non null
+		assertThat(testLegume).isNotNull();
+		// test id null comme dans le test
+		assertThat(testLegume.getIdLegume()).isNull();
+		// test si les références sont égales
+		assertThat(testLegume).isSameAs(new Legume(null, null, 0, 0, null, false, false, null));
+		// test si tous les champs sont ok par rapport au premier argument
+		assertThat(testLegume).usingRecursiveComparison().isEqualTo(new Legume (null, null, 0, 0, null, false, false, null));
+		
+		// Verifie une fois l'utilisation du repo mocked avec un legume mocked
+		Mockito.verify(mockedLegumeRepo, times(1)).save(Mockito.any(Legume.class));
+	}
+	
+	
 }
