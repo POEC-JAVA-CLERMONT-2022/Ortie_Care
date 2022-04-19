4dbaadf0ea570cbc4a63f53945ca22849db19a34
Olivia <laconte.olivia@sfr.fr>
2022-04-19 16:56:25 +0200

    controller sol

--- src/main/java/com/ipme/ortiecare/controller/SolController.java
+++ src/main/java/com/ipme/ortiecare/controller/SolController.java
@@ -1,5 +1,30 @@
 package com.ipme.ortiecare.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ipme.ortiecare.services.SolService;
+
+@Controller
 public class SolController {
 
+	@Autowired
+	private SolService solService;
+	
+	@RequestMapping("/sol")
+	public ModelAndView getSol(HttpSession session) {
+		ModelAndView mAV = new ModelAndView("sol/list-sol");
+		
+		mAV.addObject("sol", solService.findAll());
+	
+		
+		return mAV;
+	}
+	
+	
 }
