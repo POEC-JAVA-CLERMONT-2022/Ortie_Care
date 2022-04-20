14ca460c7e4f2387564ff267d036d1bddc360db1
DESKTOP-K28699H\CGYORI-Vacances <charles.gyori@gmail.com>
2022-04-20 10:54:46 +0200

    controller conseil de culture

--- src/main/java/com/ipme/ortiecare/controller/ConseilDeCultureController.java
+++ src/main/java/com/ipme/ortiecare/controller/ConseilDeCultureController.java
@@ -6,49 +6,63 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ipme.ortiecare.model.ConseilDeCulture;
+import com.ipme.ortiecare.model.Legume;
 import com.ipme.ortiecare.services.ConseilDeCultureService;
 
 @Controller
+@RequestMapping("/conseilsDeCulture")
 public class ConseilDeCultureController {
 	
 	@Autowired
 	private ConseilDeCultureService conseilService;
 	
-	@RequestMapping("/conseilsDeCulture")
+	@GetMapping("/conseilsDeCulture")
 	public ModelAndView getConseils(HttpSession session)
 	{
-		ModelAndView mAV = new ModelAndView("conseilDeCulture/liste-conseils");
+		ModelAndView mAV = new ModelAndView("conseilsDeCulture/liste-conseils");
 		mAV.addObject("conseilsDeCulture", conseilService.findAll());
+		mAV.setViewName("listeConseil");
 		return mAV;
 	}
-	@RequestMapping("/conseilsDeCulture/loltest")
+	@GetMapping("/conseilsDeCulture/loltest")
 	public String getTest()
 	{
 		return "megalol";
 	}
 	
 	
-	@RequestMapping("/conseilsDeCulture/search")
+	@GetMapping("/conseilsDeCulture/search")
 	public ModelAndView getConseils(@RequestParam("search")String boutDesc)
 	{
 		ModelAndView mAV = new ModelAndView("conseilDeCulture/liste-conseils");
 		mAV.addObject("conseilDeCulture", conseilService.findByDescriptionContaining(boutDesc));
-		mAV.addObject("search", boutDesc);
-		
+		mAV.setViewName("search");
 		return mAV;
 	}
 	
-	@RequestMapping("/conseilsDeCulture/legume")
+	@GetMapping("/conseilsDeCulture/legume")
 	public ModelAndView getConseils(@RequestParam("legume") UUID idLegume)
 	{
 		ModelAndView mAV = new ModelAndView("conseilDeCulture/liste-conseils");
 		mAV.addObject("conseilDeCulture", conseilService.findListeConseilPourLegume(idLegume));
-		mAV.addObject("legume", idLegume);
+		mAV.setViewName("conseilLegume");
+		
+		return mAV;
+	}
+	@PostMapping("/conseilsDeCulture/addConseil")
+	public ModelAndView addConseils(@RequestParam("legume") ConseilDeCulture nouveauConseil)
+	{
+		ModelAndView mAV = new ModelAndView("conseilDeCulture/liste-conseils");
+		mAV.addObject("conseilDeCulture", conseilService.create("testTitre", "testDescription"));
+		mAV.setViewName("addConseil");
 		
 		return mAV;
 	}
