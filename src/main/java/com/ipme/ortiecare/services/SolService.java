d4de19d835cdfb9a4320171e76edc0f65fb2d2c7
Olivia <laconte.olivia@sfr.fr>
2022-04-21 16:23:55 +0200

    Logger solService

--- src/main/java/com/ipme/ortiecare/services/SolService.java
+++ src/main/java/com/ipme/ortiecare/services/SolService.java
@@ -1,8 +1,11 @@
 package com.ipme.ortiecare.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +15,11 @@ import com.ipme.ortiecare.repository.SolRepository;
 @Service
 public class SolService {
 	
+	Logger logger = LoggerFactory.getLogger(Sol.class);
+	
 	@Autowired
 	private SolRepository solRepo;
-
+	
 	public SolService (SolRepository solRepo)
 	{
 		this.solRepo = solRepo;
@@ -34,25 +39,40 @@ public class SolService {
 
 	public Sol findById(UUID id) 
 	{
-		return this.solRepo.getById(id);
+		if(id != null && id.toString() != "") {
+			logger.info("La recherche a fonctionné pour l'id : " + id);
+			return solRepo.getById(id);
+		}
+		else {
+			logger.warn("La recherche n'a pas fonctionné. Renvoi d'un objet Sol vide. Valeur du paramètre passé : " + id);
+			return new Sol();
+		}
 	}
 	
 	public List<Sol> findByStructureSolContaining (String boutStructureSol)
 	{
-		return this.solRepo.findByStructureSolContaining(boutStructureSol);
+		if(boutStructureSol != null && boutStructureSol != "") {
+			logger.info("Recherche par bout de structure de sol : " + boutStructureSol);
+			return this.solRepo.findByStructureSolContaining(boutStructureSol);
+		}
+		else {
+			logger.warn("Recherche de structure échouée : retour d'une liste vide");
+			return new ArrayList<Sol>();
+		}
+		
 	}
 	
 	public List<Sol> findByTextureSolContaining (String boutTextureSol)
 	{
-		return this.solRepo.findByTextureSolContaining(boutTextureSol);
+		if(boutTextureSol != null && boutTextureSol != "") {
+			logger.info("Recherche par bout de texture sol : " + boutTextureSol);
+			return this.solRepo.findByTextureSolContaining(boutTextureSol);
+		}
+		else {
+			logger.warn("Recherche de texture échouée : retour d'une liste vide");
+			return new ArrayList<Sol>();
+		}
+		
 	}
-	
-	public Sol findByUUID(UUID id)
-	{
-		return this.solRepo.findByUUID(id);
-	}
-
-	
-
 	
 }
