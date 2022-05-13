7c0c471c42221902ef0d95dbe947e7946a9e07dd
Olivia <laconte.olivia@sfr.fr>
2022-05-13 09:11:57 +0200

    ajout CrossOrigin

--- src/main/java/com/ipme/ortiecare/controllerRest/UserControllerRest.java
+++ src/main/java/com/ipme/ortiecare/controllerRest/UserControllerRest.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.ipme.ortiecare.model.User;
 import com.ipme.ortiecare.services.UserService;
 import com.ipme.ortiecare.services.DTO.UserDTO;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users")
 public class UserControllerRest {
